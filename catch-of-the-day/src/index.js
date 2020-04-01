f8d42bb2c5c086a086c17cc606c30471dd07250e
Plengpai Ratnajarn <pinplengpai@gmail.com>
2020-04-01 12:34:22 +0700

    add first component

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,3 +1,10 @@
-// let's go
-alert('hey');
-console.log('yayyyy');
+import React from 'react'; 
+import { render } from 'react-dom';
+
+class StorePicker extends React.Component{
+    render() {
+        return <p>hello</p>
+    }
+}
+
+render(<p>Heyyy</p>, document.querySelector('#main'));
