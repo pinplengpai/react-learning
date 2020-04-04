bf90ceb7e48bc56d22dfab3d0ebff1bb5f49c0ba
Plengpai Ratnajarn <pinplengpai@gmail.com>
2020-04-04 21:37:43 +0700

    Events, ref and this binding

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -2,15 +2,26 @@ import React, { Fragment } from 'react'; //make ingredient
 import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component { //import the sausages 
+
+    myInput = React.createRef();
+
+    goToStore = event => {
+        //1.stop the form from submitting
+        event.preventDefault(); 
+        //2.get the text from the input.
+        console.log(this); //it will show the instance of each component
+
+        //3.Change the page to /store/whatever-they-entered
+    }
+
     render() { 
-        console.log('this');
         return (
             <Fragment>
-                <form className="store-selector">
+                <form className="store-selector" onSubmit={this.goToStore}>
                     <h2>Please enter a store</h2>
-                    <button class="button"> Click me!</button>
                     <input 
                     type="text" 
+                    ref={this.myInput}
                     required placeholder="Store Name" 
                     defaultValue={getFunName()} 
                     />
