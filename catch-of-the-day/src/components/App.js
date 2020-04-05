b1b19c1328095738489d3ebc0d16225e165aae84
Plengpai Ratnajarn <pinplengpai@gmail.com>
2020-04-05 10:11:15 +0700

    loading data into a state

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "./Header";
 import Order from "./Order";
 import Inventory from "./Inventory";
+import sampleFishes from "../sample-fishes";
 
 class App extends React.Component {
     state = {
@@ -25,6 +26,9 @@ class App extends React.Component {
         //this.state.fishes.fish1 = fish; it's ususally write in javascript
     }; 
 
+    loadSampleFishes = () => {
+        this.setState({ fishes : sampleFishes })
+    };
 
     render(){
         return (
@@ -33,7 +37,9 @@ class App extends React.Component {
                     <Header tagline="Fresh Seafood Market" />
                 </div>
                 <Order />
-                <Inventory addFish={this.addFish} />
+                <Inventory addFish={this.addFish} 
+                loadSampleFishes={this.loadSampleFishes } 
+                />
             </div>
         );
     }
